876c0112a70ae912f46e1ae5805fc95b388e541b
bocheng24 <jeffcgp@hotmail.com>
2022-01-05 14:15:31 +0800

    setup heritance template

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.get('/', (req, res, next) => {
-    res.status(200).render('home');
+    const payload = {
+        pageTitle: 'Home'
+    }
+
+    res.status(200).render('home', payload);
 })
